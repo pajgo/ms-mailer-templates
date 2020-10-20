bfa1f56e66b90376d1986d8540888980c229e11b
Alexey Khoroshev <aleksxor@gmail.com>
2020-10-20 17:07:04 +0300

    feat(cpst): add new activation email template

--- test/index.js
+++ test/index.js
@@ -29,7 +29,7 @@ describe('TemplateSuite', function testSuite() {
       .then(function inspectError(promise) {
         expect(promise.isRejected()).to.be.eq(false);
         expect(promise.isFulfilled()).to.be.eq(true);
-        expect(promise.value()).to.match(/Confirm email address/m);
+        expect(promise.value()).to.match(/Activate email address/m);
       });
   });
 
@@ -39,7 +39,7 @@ describe('TemplateSuite', function testSuite() {
       .then(function inspectError(promise) {
         expect(promise.isRejected()).to.be.eq(false);
         expect(promise.isFulfilled()).to.be.eq(true);
-        expect(promise.value()).to.match(/Confirm email address/m);
+        expect(promise.value()).to.match(/Activate email address/m);
       });
   });
 });
